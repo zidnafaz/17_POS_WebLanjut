0e188754b44ebb1eea4442632ce6b15096249fb9
Zidna Billah Faza <zidnafaza27@gmail.com>
2025-03-17 21:48:16 +0700

    Jobsheet05-Prak1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,49 +1,74 @@
-{{-- -- resources/views/layouts/app.blade.php -- --}}
+@extends('adminlte::page')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">POS</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('products') || Request::is('category/*') ? 'active' : '' }}" href="{{ route('products.index') }}">Products</a>
-                    </li>
-                    {{-- <li class="nav-item">
-                        <a class="nav-link {{ Request::is('user/*') ? 'active' : '' }}" href="{{ url('/user/1/name/Faza') }}">User</a>
-                    </li> --}}
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('user') ? 'active' : '' }}" href="{{ url('/user') }}">User</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('user_count') ? 'active' : '' }}" href="{{ url('/user_count') }}">Jumlah User</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('penjualan') ? 'active' : '' }}" href="{{ route('selling.index') }}">Penjualan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link {{ Request::is('Level') ? 'active' : '' }}" href="{{ url('/Level') }}">Level</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container mt-4">
-        @yield('content')
+{{-- Extend and customize the browser title --}}
+
+@section('title')
+    {{ config('adminlte.title') }}
+    @hasSection('subtitle') | @yield('subtitle') @endif
+@stop
+
+{{-- Extend and customize the page content header --}}
+
+@section('content_header')
+    @hasSection('content_header_title')
+        <h1 class="text-muted">
+            @yield('content_header_title')
+
+            @hasSection('content_header_subtitle')
+                <small class="text-dark">
+                    <i class="fas fa-xs fa-angle-right text-muted"></i>
+                    @yield('content_header_subtitle')
+                </small>
+            @endif
+        </h1>
+    @endif
+@stop
+
+{{-- Rename section content to content_body --}}
+
+@section('content')
+    @yield('content_body')
+@stop
+
+{{-- Create a common footer --}}
+
+@section('footer')
+    <div class="float-right">
+        Version: {{ config('app.version', '1.0.0') }}
     </div>
-</body>
-</html>
+
+    <strong>
+        <a href="{{ config('app.company_url', '#') }}">
+            {{ config('app.company_name', 'My company') }}
+        </a>
+    </strong>
+@stop
+
+{{-- Add common Javascript/Jquery code --}}
+
+@push('js')
+<script>
+
+    $(document).ready(function() {
+        // Add your common script logic here...
+    });
+
+</script>
+@endpush
+
+{{-- Add common CSS customizations --}}
+
+@push('css')
+<style type="text/css">
+
+    {{-- You can add AdminLTE customizations here --}}
+    /*
+    .card-header {
+        border-bottom: none;
+    }
+    .card-title {
+        font-weight: 600;
+    }
+    */
+</style>
+@endpush
