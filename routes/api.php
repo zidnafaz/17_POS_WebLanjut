9edcdb428b1f211373b0edebc8c8f63ec48dd77b
Zidna Billah Faza <zidnafaza27@gmail.com>
2025-05-08 08:11:33 +0700

    Jobsheet08-P2-API Login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/register', RegisterController::class)->name('register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
